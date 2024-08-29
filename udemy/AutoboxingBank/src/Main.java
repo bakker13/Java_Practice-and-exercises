789bf22714a640704520c481c92eba250acef74f
bakker13 <bakkrobi@gmail.com>
2024-08-29 14:54:47 +0200

    Delete Customer class, adding a record instead of using a class

--- udemy/AutoboxingBank/src/Main.java
+++ udemy/AutoboxingBank/src/Main.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+
+record Customer (String name, ArrayList<Double> transactions) {
+    public Customer(String name, double initialDeposit) {
+        this(name.toUpperCase(), new ArrayList<Double>(500));
+        transactions.add(initialDeposit);
+    }
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        }
+    }
 }
